9fc239f6e21e4ecc37ee1690ff2250061320afa4
Antonio1702 <ing.antoniopz@gmail.com>
2019-05-04 14:18:05 -0500

    actualizaciones

--- server/index.js
+++ server/index.js
@@ -15,5 +15,15 @@ require('./routes/gatosRoutes')(app);
 /*app.get('/', (req, res) => {
     res.send('si funciona');
 })*/
-app.listen(process.env.PORT || 5000);
+
 //alt 124 para pibe
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/buld'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    });
+}
+
+app.listen(process.env.PORT || 5000);
