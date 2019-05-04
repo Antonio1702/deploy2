b574058ca1f9342a3bac65da804b7e51e0ccb2ab
Antonio1702 <ing.antoniopz@gmail.com>
2019-05-04 13:43:44 -0500

    first commit

---
+++ server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_URL: 'mongodb://usuario:USer@cluster0-shard-00-00-1rzna.mongodb.net:27017,cluster0-shard-00-01-1rzna.mongodb.net:27017,cluster0-shard-00-02-1rzna.mongodb.net:27017/deploy?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+}
---
+++ server/routes/gatosRoutes.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const Gato = mongoose.model ('gatos');
+
+module.exports = (app) => {
+    app.get ('/api/gatos', async (req, res) => {
+        const gatos = await Gato.find({});
+        res.send(gatos);
+    })
+}
